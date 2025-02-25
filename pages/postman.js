ffb80900cdd5d453ebe283e3325facae3ab371d7
Ryley Johnson <ryleyjohnsonemail@gmail.com>
2025-02-24 23:16:22 -0800

    postman page tests and pom refactored static props

--- pages/postman.js
+++ pages/postman.js
@@ -1,70 +1,21 @@
-exports.Postman = class Postman {
-    constructor (page) {
+class PostmanPage {
+    constructor(page) {
         this.page = page;
-        this.topGithubBt = this.page.locator('a[href="https://github.com/MRJOHN5ON/postmanAPI_testing"]').first()
-        this.bottomGithubBt = this.page.locator('a[href="https://github.com/MRJOHN5ON/postmanAPI_testing"]').last()
-        this.postmanLogo = this.page.getByAltText('Logo')
-        this.googleSheetslink = this.page.getByRole('link', { name: 'Google Sheets Test Case Data'})
-        this.req1Img = this.page.getByAltText('Adding Products to a Kit')
-        this.nodeTestsLink = this.page.getByRole('link', {name : 'Click here!'})
-        this.postman1Img = this.page.getByAltText('Exceeding 30 Items')
-        this.jira1Img = this.page.getByAltText('Bug Report in JIRA').first()
-        this.postman2Img = this.page.getByAltText('Non-existent Product IDs')
-        this.jira2Img = this.page.getByAltText('Bug Report in JIRA').last()
-        this.req2Pdf = this.page.getByRole('link', {name: 'Detailed Requirements PDF'})
-        this.resultsTable = this.page.getByAltText('Results')
-        this.footerHomeButton = this.page.getByRole('link', {name: 'Home Page'})
-        this.footerProjectBankButton = this.page.getByRole('link', {name: 'Project Bank'})
-        this.emailLink = this.page.getByRole('link', {name: 'ryleyjohnsonemail@gmail.com'})
     }
 
-    async goToPostmanProject() {
-        await this.page.goto('https://mrjohn5on.github.io/project1.html')
-    }
-
-    async clickTopGithubBt() {
-        await this.topGithubBt.click()
-    }
-    async clickBottomGithubBt() {
-        await this.bottomGithubBt.click()
-    }
-   
-    async clickGoogleSheets() {
-        await this.googleSheetslink.click()
-    }
-    async clickReq1Img() {
-        await this.req1Img.click()
-    }
-    async clickNodeTestsLink() {
-        await this.nodeTestsLink.click()
-    }
-    async clickPostman1Img() {
-        await this.postman1Img.click()
-    }
-    async clickJira1Img() {
-        await this.jira1Img.click()
-    }
-    async clickPostman2Img() {
-        await this.postman2Img.click()
-    }
-    async clickJira2Img() {
-        await this.jira2Img.click()
-    }
-    async clickReq2Pdf() {
-        await this.req2Pdf.click()
-    }
-    async clickResultsTable() {
-        await this.resultsTable.click()
-    }
-    async clickFooterHomeButton() {
-        await this.footerHomeButton.click()
-    }
-    async clickFooterProjectBankButton() {
-        await this.footerProjectBankButton.click()
-    }
-    async clickEmailLink() {
-        await this.emailLink.click()
-    }
-
-   
+    static githubBt = 'a[href="https://github.com/MRJOHN5ON/postmanAPI_testing"]';
+    static postmanLogoAltText = 'Logo';
+    static googleSheetsRoleName = 'Google Sheets Test Case Data';
+    static req1ImgAltText = 'Adding Products to a Kit';
+    static nodeTestLinkRoleName = 'Click here!';
+    static postman1ImgAltText = 'Exceeding 30 Items';
+    static jiraImgAltText = 'Bug Report in JIRA';
+    static postman2ImgAltText = 'Non-existent Product IDs';
+    static req2PdfRoleName = 'Detailed Requirements PDF';
+    static resultsTableAltText = 'Results';
+    static footerHomeButtonRoleName = 'Home Page';
+    static footerProjectBankButtonRoleName = 'Project Bank';
+    static emailLinkRoleName = 'ryleyjohnsonemail@gmail.com';
 }
+
+export default PostmanPage;
--- tests/postmanPage.spec.js
+++ tests/postmanPage.spec.js
@@ -1,238 +1,244 @@
 import { test, expect } from '@playwright/test';
-import { Postman } from '../pages/postman.js';
+import PostmanPage from '../pages/postman.js';
 
 test.describe('Page structure', () => {
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://mrjohn5on.github.io/project1.html');
+    });
+
     test('should display the correct page title', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
         await expect(page).toHaveTitle('API Testing with Postman');
     });
 
     test('all h2 should be #ff0404 color', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-
         const h2Headers = page.locator('h2');
-
         const countH2 = await h2Headers.count();
 
         for (let i = 0; i < countH2; i++) {
             await expect(h2Headers.nth(i)).toHaveCSS('color', 'rgb(255, 4, 4)');
-
-
         }
-
     });
 
     test('all h3 headers should be #a6ff00 color', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
         const h3Headers = page.locator('h3');
         const count = await h3Headers.count();
+
         for (let i = 0; i < count; i++) {
             await expect(h3Headers.nth(i)).toHaveCSS('color', 'rgb(166, 255, 0)');
         }
-
     });
 
     test('all h4 headers should be #ff0404 color', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
         const h4Headers = page.locator('h4');
         const count = await h4Headers.count();
+
         for (let i = 0; i < count; i++) {
             await expect(h4Headers.nth(i)).toHaveCSS('color', 'rgb(255, 4, 4)');
         }
     });
 
     test('all p class image captions should be #a6ff00 color', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
         const pClassImageCaptions = page.locator('p.image-caption');
         const count = await pClassImageCaptions.count();
+
         for (let i = 0; i < count; i++) {
             await expect(pClassImageCaptions.nth(i)).toHaveCSS('color', 'rgb(166, 255, 0)');
         }
     });
-
 });
 
 test.describe('Link Tests', () => {
-
-
-    test('gitHub links open in new tab', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        const [topGithubBt, bottomGithubBt] = await Promise.all([
-            context.waitForEvent('page'),
-            context.waitForEvent('page'),
-            postmanPage.clickTopGithubBt(),
-            postmanPage.clickBottomGithubBt()
-        ]);
-        await topGithubBt.waitForLoadState();
-        await bottomGithubBt.waitForLoadState();
-        await expect(topGithubBt).toHaveURL('https://github.com/MRJOHN5ON/postmanAPI_testing')
-        await expect(bottomGithubBt).toHaveURL('https://github.com/MRJOHN5ON/postmanAPI_testing')
+    test.beforeEach(async ({ page }) => {
+        await page.goto('https://mrjohn5on.github.io/project1.html');
     });
 
+    test('gitHub links open in new tab', async ({ page, context }) => {
+        const topGithubPage = context.waitForEvent('page');
+        await page.locator(PostmanPage.githubBt).first().click();
+        const topGithubBt = await topGithubPage;
 
+        const bottomGithubPage = context.waitForEvent('page');
+        await page.locator(PostmanPage.githubBt).last().click();
+        const bottomGithubBt = await bottomGithubPage;
+
+        await topGithubBt.waitForLoadState();
+        await bottomGithubBt.waitForLoadState();
+
+        await expect(topGithubBt).toHaveURL('https://github.com/MRJOHN5ON/postmanAPI_testing');
+        await expect(bottomGithubBt).toHaveURL('https://github.com/MRJOHN5ON/postmanAPI_testing');
+    });
 
     test('Google Sheets link opens in new tab', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        const [googleSheets] = await Promise.all([
-            context.waitForEvent('page'),
-            postmanPage.clickGoogleSheets()
-        ])
+        const googleSheetsPage = context.waitForEvent('page');
+        await page.getByRole('link', { name: PostmanPage.googleSheetsRoleName }).click();
+        const googleSheets = await googleSheetsPage;
+
         await googleSheets.waitForLoadState();
         await expect(googleSheets).toHaveURL('https://docs.google.com/spreadsheets/d/1u7IGVb-FNyHqHK7GsBrwKLk84MCGCfISmwzFId9o3K0/edit?gid=0#gid=0');
     });
 
     test('req1Img link opens in new tab with an enlarged image', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.req1Img).toBeVisible();
+        const req1Img = page.getByAltText(PostmanPage.req1ImgAltText);
+        await expect(req1Img).toBeVisible();
 
-
-        const imgSizeSmall = await postmanPage.req1Img.boundingBox();
+        const imgSizeSmall = await req1Img.boundingBox();
         expect(imgSizeSmall).toBeTruthy();
 
-
-        postmanPage.clickReq1Img();
+        await req1Img.click();
         const newTab = await context.waitForEvent('page');
 
         await newTab.waitForLoadState();
         await expect(newTab).toHaveURL('https://mrjohn5on.github.io/assets/images/p1.png');
 
-
         const imgSizeLarge = await newTab.locator('img').boundingBox();
         expect(imgSizeLarge).toBeTruthy();
-
 
         expect(imgSizeLarge.width).toBeGreaterThan(imgSizeSmall.width);
         expect(imgSizeLarge.height).toBeGreaterThan(imgSizeSmall.height);
     });
 
     test('node-tests link opens in new tab', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        const [nodeTests] = await Promise.all([
-            context.waitForEvent('page'),
-            postmanPage.clickNodeTestsLink()
-        ]);
+        const nodeTestsPage = context.waitForEvent('page');
+        await page.getByRole('link', { name: PostmanPage.nodeTestLinkRoleName }).click();
+        const nodeTests = await nodeTestsPage;
+
         await nodeTests.waitForLoadState();
         await expect(nodeTests).toHaveURL('https://github.com/MRJOHN5ON/postmanAPI_testing/tree/main/node-tests');
     });
 
     test('postman1Img link opens in new tab as an enlarged image', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.postman1Img).toBeVisible();
-        const imgSizeSmall = await postmanPage.postman1Img.boundingBox();
+        const postman1Img = page.getByAltText(PostmanPage.postman1ImgAltText);
+        await expect(postman1Img).toBeVisible();
+
+        const imgSizeSmall = await postman1Img.boundingBox();
         expect(imgSizeSmall).toBeTruthy();
-        postmanPage.clickPostman1Img();
+
+        await postman1Img.click();
         const newTab = await context.waitForEvent('page');
+
         await newTab.waitForLoadState();
         await expect(newTab).toHaveURL('https://mrjohn5on.github.io/assets/images/p2.png');
+
         const imgSizeLarge = await newTab.locator('img').boundingBox();
         expect(imgSizeLarge).toBeTruthy();
+
         expect(imgSizeLarge.width).toBeGreaterThan(imgSizeSmall.width);
         expect(imgSizeLarge.height).toBeGreaterThan(imgSizeSmall.height);
     });
 
     test('jira1Img link opens in new tab as an enlarged image', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.jira1Img).toBeVisible();
-        const imgSizeSmall = await postmanPage.jira1Img.boundingBox();
+        const jira1Img = page.getByAltText(PostmanPage.jiraImgAltText).first();
+        await expect(jira1Img).toBeVisible();
+
+        const imgSizeSmall = await jira1Img.boundingBox();
         expect(imgSizeSmall).toBeTruthy();
-        postmanPage.clickJira1Img();
+
+        await jira1Img.click();
         const newTab = await context.waitForEvent('page');
+
+
+
         await newTab.waitForLoadState();
         await expect(newTab).toHaveURL('https://mrjohn5on.github.io/assets/images/p3.png');
+
         const imgSizeLarge = await newTab.locator('img').boundingBox();
         expect(imgSizeLarge).toBeTruthy();
+
         expect(imgSizeLarge.width).toBeGreaterThan(imgSizeSmall.width);
         expect(imgSizeLarge.height).toBeGreaterThan(imgSizeSmall.height);
     });
 
     test('postman2Img link opens in new tab as an enlarged image', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.postman2Img).toBeVisible();
-        const imgSizeSmall = await postmanPage.postman2Img.boundingBox();
+        const postman2Img = page.getByAltText(PostmanPage.postman2ImgAltText);
+        await expect(postman2Img).toBeVisible();
+
+        const imgSizeSmall = await postman2Img.boundingBox();
         expect(imgSizeSmall).toBeTruthy();
-        postmanPage.clickPostman2Img();
+
+        await postman2Img.click();
         const newTab = await context.waitForEvent('page');
+
+
+
         await newTab.waitForLoadState();
         await expect(newTab).toHaveURL('https://mrjohn5on.github.io/assets/images/p4.png');
+
         const imgSizeLarge = await newTab.locator('img').boundingBox();
         expect(imgSizeLarge).toBeTruthy();
+
         expect(imgSizeLarge.width).toBeGreaterThan(imgSizeSmall.width);
         expect(imgSizeLarge.height).toBeGreaterThan(imgSizeSmall.height);
     });
 
     test('jira2Img link opens in new tab as an enlarged image', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.jira2Img).toBeVisible();
-        const imgSizeSmall = await postmanPage.jira2Img.boundingBox();
+        const jira2Img = page.getByAltText(PostmanPage.jiraImgAltText).last();
+        await expect(jira2Img).toBeVisible();
+
+        const imgSizeSmall = await jira2Img.boundingBox();
         expect(imgSizeSmall).toBeTruthy();
-        postmanPage.clickJira2Img();
+
+        await jira2Img.click();
         const newTab = await context.waitForEvent('page');
+
+
+
         await newTab.waitForLoadState();
         await expect(newTab).toHaveURL('https://mrjohn5on.github.io/assets/images/p5.png');
+
         const imgSizeLarge = await newTab.locator('img').boundingBox();
         expect(imgSizeLarge).toBeTruthy();
+
         expect(imgSizeLarge.width).toBeGreaterThan(imgSizeSmall.width);
         expect(imgSizeLarge.height).toBeGreaterThan(imgSizeSmall.height);
     });
-    test('req2pdf link triggers a PDF download', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.req2Pdf).toBeVisible();
 
-        const [download] = await Promise.all([
-            page.waitForEvent('download'),
-            postmanPage.clickReq2Pdf(),
-        ]);
+    test('req2pdf link triggers a PDF download', async ({ page }) => {
+        const req2Pdf = page.getByRole('link', { name: PostmanPage.req2PdfRoleName });
+        await expect(req2Pdf).toBeVisible();
+
+        await req2Pdf.click();
+        const download = await page.waitForEvent('download');
+
 
 
         expect(download.url()).toBe('https://practicum-content.s3.us-west-1.amazonaws.com/qa-us/pdf/Requirements_Shipping_Price_Calculations.pdf');
     });
+
     test('results img link opens in new tab as an enlarged image', async ({ page, context }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.resultsTable).toBeVisible();
-        const imgSizeSmall = await postmanPage.resultsTable.boundingBox();
+        const resultsTable = page.getByAltText(PostmanPage.resultsTableAltText);
+        await expect(resultsTable).toBeVisible();
+
+        const imgSizeSmall = await resultsTable.boundingBox();
         expect(imgSizeSmall).toBeTruthy();
-        postmanPage.clickResultsTable();
+
+        await resultsTable.click();
         const newTab = await context.waitForEvent('page');
+
+
+
         await newTab.waitForLoadState();
         await expect(newTab).toHaveURL('https://mrjohn5on.github.io/assets/images/p6.png');
+
         const imgSizeLarge = await newTab.locator('img').boundingBox();
         expect(imgSizeLarge).toBeTruthy();
+
         expect(imgSizeLarge.width).toBeGreaterThan(imgSizeSmall.width);
         expect(imgSizeLarge.height).toBeGreaterThan(imgSizeSmall.height);
     });
 
     test('Footer home button navigates to the home page', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.footerHomeButton).toBeVisible();
-        await postmanPage.clickFooterHomeButton();
+        const footerHomeButton = page.getByRole('link', { name: PostmanPage.footerHomeButtonRoleName });
+        await expect(footerHomeButton).toBeVisible();
+
+        await footerHomeButton.click();
         await expect(page).toHaveURL('https://mrjohn5on.github.io/index.html');
     });
 
     test('Footer project bank button navigates to the project bank page', async ({ page }) => {
-        const postmanPage = new Postman(page);
-        await postmanPage.goToPostmanProject();
-        await expect(postmanPage.footerProjectBankButton).toBeVisible();
-        await postmanPage.clickFooterProjectBankButton();
+        const footerProjectBankButton = page.getByRole('link', { name: PostmanPage.footerProjectBankButtonRoleName });
+        await expect(footerProjectBankButton).toBeVisible();
+
+        await footerProjectBankButton.click();
         await expect(page).toHaveURL('https://mrjohn5on.github.io/#projects');
     });
-
 });
-
-
-
